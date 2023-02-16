048113085e000ad328e132b7dbab068cfa13bcf5
Guilherme Dantas Maranhão <guilherme.maranhao2@gmail.com>
2023-02-16 11:17:11 -0300

    Progresso Projeto React Costs

---
+++ React/Projetos React/projeto-react-costs/meu-projeto/costs/src/components/layout/Loading.js
@@ -0,0 +1,14 @@
+import styles from './Loading.module.css'
+
+
+
+function Loading() {
+    return(
+        <>
+        
+        </>
+    )
+
+}
+
+export default Loading
--- React/Projetos React/projeto-react-costs/meu-projeto/costs/src/components/pages/Projeto.js
+++ React/Projetos React/projeto-react-costs/meu-projeto/costs/src/components/pages/Projeto.js
@@ -1,8 +1,34 @@
 import { useLocation } from "react-router-dom"
+import {useState, useEffect} from 'react'
 
 import Message from "../layout/Message"
+import Container from '../layout/Container'
+import LinkButton from '../layout/LinkButton'
+import ProjetoCard from '../projeto/ProjetoCard'
+
+import styles from './Projeto.module.css'
 
 function Projeto () {
+
+    const [projects, setProjects] = useState([])
+
+    useEffect(() => {
+
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data)
+            setProjects(data)
+        })
+        .catch(err => console.log(err))
+    }, [])
+
+
     //uso do hook location
     const location = useLocation()
     //inicializo com uma mensagem vazia
@@ -12,9 +38,25 @@ function Projeto () {
     }
 
     return(
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.projeto_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/novoprojeto" text='Criar Projeto' />
+            </div>
             {message && <Message type="success"  text={message} />}
+
+            <Container customClass="start">
+                {projects.length > 0 &&
+                    projects.map((project) => <ProjetoCard 
+                    name={project.name}
+                    id={project.id}
+                    budget={project.budget}
+                    category={project.category.name}
+                    key={project.id}
+                    
+                     />
+                )}
+            </Container>
         </div>
     )
 }
